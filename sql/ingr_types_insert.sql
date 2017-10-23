fed54395b965c03018d06ea7643ef0b13155aac8
cz <czimmerman@infsoftware.com>
2017-10-23 12:24:29 -0400

    ingr_types_insert

---
+++ sql/ingr_types_insert.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM meals.ingr_type
+LIMIT 0, 1000
+
+-- Date: 2017-10-23 12:15
+*/
+INSERT INTO `ingr_type` (`ingr_type_id`,`ingr_type_name`) VALUES (5,'Canned/Jarred');
+INSERT INTO `ingr_type` (`ingr_type_id`,`ingr_type_name`) VALUES (7,'Deli/Prepared');
+INSERT INTO `ingr_type` (`ingr_type_id`,`ingr_type_name`) VALUES (8,'Eggs and Dairy');
+INSERT INTO `ingr_type` (`ingr_type_id`,`ingr_type_name`) VALUES (2,'Meat');
+INSERT INTO `ingr_type` (`ingr_type_id`,`ingr_type_name`) VALUES (3,'Produce');
+INSERT INTO `ingr_type` (`ingr_type_id`,`ingr_type_name`) VALUES (4,'Seafood');
+INSERT INTO `ingr_type` (`ingr_type_id`,`ingr_type_name`) VALUES (1,'Spices');
+INSERT INTO `ingr_type` (`ingr_type_id`,`ingr_type_name`) VALUES (6,'Sweeteners/Baking');
+INSERT INTO `ingr_type` (`ingr_type_id`,`ingr_type_name`) VALUES (9,'Vinegar and Oil');
